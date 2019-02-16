ff76ac6c0ae10481da73e9a14a627be5327a3aa6
Tim S <timothy_sudijono@brown.edu>
2019-02-16 15:25:50 -0500

    updated continuous time sphere model code

--- 2d_graph_example/ContinuousTimeSphereModel.py
+++ 2d_graph_example/ContinuousTimeSphereModel.py
@@ -1,12 +1,26 @@
 import numpy as np 
 import intervaltree as it
 import scipy.spatial as sp
+import SphereExample as sphere
 
 def get_sensor_lifetimes(time, birth_rate, death_rate):
-	'''
-	A shoddy implementation of a queueing model, with exponentially distributed processing time and
-	exponentially distributed growth.
-	'''
+	"""
+	A shoddy implementation of a queueing model.
+	'births' serves as the queue.
+	See the ContinuousTimeSphereExample notebook for
+	details on implementation.
+
+	Parameters
+	-------------------------------------
+	time: float
+		simulate the queueing model up to this time
+	birth_rate: float > 0
+		birth rate (we expect these many to be born
+		in a unit time interval)
+	death_rate: float > 0
+		death rate
+	"""
+
 	l1 = 1/birth_rate
 	l2 = 1/death_rate
 
@@ -17,7 +31,7 @@ def get_sensor_lifetimes(time, birth_rate, death_rate):
 
 	while current_time < time:
 
-		# if there are no current points
+		# if there are no points in the queue 
 		if len(births) == 0:
 			current_time += np.random.exponential(l2)
 			births.append(current_time)
@@ -29,28 +43,32 @@ def get_sensor_lifetimes(time, birth_rate, death_rate):
 		if np.random.rand() < l1/(l1 + l2):
 			births.append(current_time)
 		else:
-			intervals[births.pop(0):current_time] = sample_uniform_sphere(1).tolist()[0]
+			intervals[births.pop(0):current_time] = \
+						sphere.sample_uniform_sphere(1).tolist()[0]
 
 	#add the remaining nodes:
 	for i in range(len(births[:-1])):
-		intervals[births[i]:time] = sample_uniform_sphere(1).tolist()[0]
+		intervals[births[i]:time] = sphere.sample_uniform_sphere(1).tolist()[0]
 
 	return intervals
 
-def create_dynamic_networks(lambda1,lambda2, obs_times, observation_function, edge_wtsfn):
-	'''
+def sample_dynamic_network(intervals, obs_times, obsfn, edge_wtsfn):
+	"""
 	Can query the interval tree below for more information about the points
-	-----------------------------------------------------------------------
-	Input:
-	lambda1: birth rate (we expect )
-	lambda2: death rate (length of a lifetime of a node)
-	obs_times - list of times at which to sample obs function
-	observation_function - function from which to sample
-	edge_wtsfn - function applied to edge wts
-	'''
 
-	### create the interval tree ###
-	intervals = get_sensor_lifetimes(max(obs_times), lambda1, lambda2)
+	Parameters
+	----------------------------
+	Input:
+	intervals: intervaltree
+		output of get_sensor_lifetimes
+	obs_times: list of floats
+		list of times at which to sample obs function
+	obsfn:
+		function from which to sample
+	edge_wtsfn:
+		function applied to edge wts
+	"""
+
 
 	### query tree at each timestep ###
 	coordinate_set = []
@@ -61,44 +79,10 @@ def create_dynamic_networks(lambda1,lambda2, obs_times, observation_function, ed
 		coordinate_set.append(coordinates)
 
 		hull = sp.ConvexHull(coordinates)
-		node_wts.append(get_node_wts(t,hull,observation_function))
+		node_wts.append(sphere.get_node_wts(t,hull,obsfn))
 		edge_wts.append(edge_wtsfn(hull))
 
-	return (node_wts,edge_wts,point_set)
+	return (node_wts,edge_wts,coordinate_set)
 
-
-
-def sample_uniform_sphere(N):
-	cds = np.random.normal(0,1,(N,3))
-	normalized_cds = cds/np.reshape(np.sqrt(np.sum(cds**2,axis = 1)),(N,1))
-	return normalized_cds
-
-def get_node_wts(t,hull_obj, obsfn):
-    hull = hull_obj
-    node_wts = [obsfn(t,p) for p in hull.points]
-    return np.array(node_wts)
-
-def get_edge_wts(hull_obj, alpha = 1.0):
-	"""
-	map edges to their distances
-	use this to create a dynamic network, edges are these distances.
-	Returns
-	-------
-	edges: scipy.sparse(N, N)
-	    A sparse matrix with the edge weights
-	alpha: float
-	    Amount by which to weight distances
-	"""
-	hull = hull_obj
-	edges = simplex_list_to_edge_list(hull.simplices)
-
-	v = hull.points
-	ds = [cartesian_to_sphere_distance(v[e[0],:], v[e[1],:]) for e in edges]
-	ds = alpha*np.array(ds + ds)
-
-	e0 = np.array([e[0] for e in edges] + [e[1] for e in edges])
-	e1 = np.array([e[1] for e in edges] + [e[0] for e in edges])
-
-	return sparse.coo_matrix((ds, (e0, e1)), shape=(len(v), len(v)))
 
 
