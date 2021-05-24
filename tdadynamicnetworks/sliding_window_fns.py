4ba7e4a353f68eb0a24e0b16a52fbe69769dcff6
tsudijon <timothysudijono@gmail.com>
2021-05-24 18:07:13 -0400

    updating imports for src files

--- tdadynamicnetworks/sliding_window_fns.py
+++ tdadynamicnetworks/sliding_window_fns.py
@@ -5,7 +5,7 @@ from __future__ import division
 import numpy as np
 import numpy.linalg as la
 from scipy.spatial.distance import squareform
-import .persistence_fns as pf
+import persistence_fns as pf
 
 
 ### TODO: allow for fractional TAU?
