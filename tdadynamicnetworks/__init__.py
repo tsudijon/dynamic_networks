0c6303813af7d10e3a97c098d87c1d0725ad2f50
tsudijon <timothysudijono@gmail.com>
2021-05-25 13:20:47 -0400

    changing init structure to include examples module

--- tdadynamicnetworks/__init__.py
+++ tdadynamicnetworks/__init__.py
@@ -2,3 +2,6 @@ from . import persistence_fns as pf
 from . import rgg_fns as rgg
 from . import sliding_window_fns as sw
 from . import wasserstein as w
+from .examples import ContinuousTimeSensorModel as ctsm
+from .examples import SphereExample as sphere
+from .examples import PlaneExample as plane
